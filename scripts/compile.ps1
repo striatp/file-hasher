072248805f7465f0452c4e69f24eee1a5c015144
Striatp <sebastien.works@outlook.com>
2025-11-22 13:37:29 +0100

    chore: reduce size of the script and remove parameter ♻️

--- scripts/compile.ps1
+++ scripts/compile.ps1
@@ -1,52 +1,27 @@
 #!/usr/bin/env pwsh
 
-param (
-    [Parameter(Mandatory = $false)]
-    [System.String] $Source = "./source/main.c",
+param ()
 
-    [Parameter(Mandatory = $false)]
-    [System.String] $Output = "./build/output.exe"
-)
-
-if (-not $Source) {
-    Write-Host "[ERROR] " -NoNewline -ForegroundColor Red ;
-    Write-Host "A source file must be specified for compilation to occur." ;
-    exit 1 ;
-}
-
-if (-not $Output) {
-    Write-Host "[ERROR] " -NoNewline -ForegroundColor Red ;
-    Write-Host "An output file must be specified for compilation to occur." ;
-    exit 1 ;
-}
-
-if (-not (Test-Path -Path $Source)) {
+if (-not (Test-Path -Path "source/main.c")) {
     Write-Host "[ERROR] " -NoNewline -ForegroundColor Red ;
     Write-Host "The specified source file does not exist." ;
     exit 1 ;
 }
 
-#if (-not (Test-Path -Path $Output)) {
-#    Write-Host "[ERROR] " -NoNewline -ForegroundColor Red ;
-#    Write-Host "The specified output file does not exist." ;
-#    exit 1 ;
-#}
-
 function Compile {
     [CmdletBinding()]
     [OutputType([System.Void])]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string] $Source,
+    param ()
 
-        [Parameter(Mandatory = $true)]
-        [string] $Output
+    if (-not (Test-Path -Path "obj")) {
+        New-Item -ItemType Directory -Path "./" -Name "obj" ;
+    }
 
-    )
+    nasm -f win64 source/crc32_step.asm -o obj/crc32_step.obj ;
+    clang source/main.c source/crc32.c obj/crc32_step.obj -o build/hasher.exe ;
 
-    gcc $Source -o $Output ;
-
-    Write-Host "Compiled" ;
+    Write-Host "[SUCCESS] " -NoNewline -ForegroundColor Green ;
+    Write-Host "Successfully compiled the main.c source file." ;
 }
 
-Compile -Source $Source -Output $Output ;
+Compile ;
